4d704964874ffdc1304a4705e9bfda78dbd066ef
BrycePearce <bpearce@angelo.edu>
2017-03-28 23:55:04 -0500

    Removed dotenv key for ease of use. Formatted.

--- README.md
+++ README.md
@@ -8,6 +8,3 @@ Usage:
 node app.js -a 79760
 
 node app.js -a '1st street, Washington DC, USA'
-
-
-**Note: you will need to provide your own api key using [dotenv](https://www.npmjs.com/package/dotenv)
--- app.js
+++ app.js
@@ -1,41 +1,42 @@
 'use strict';
 const yargs = require('yargs');
 const axios = require('axios');
+const query = "96139e9c46e92a3c8641cc5dc6dcf1ea";
 require('dotenv').config()
 
 const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
 var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress;
 
 axios.get(geocodeUrl).then((response) => {
-    if (response.data.status === 'ZERO_RESULTS') {
-        throw new Error('Unable to find that address.');
-    }
-    var lat = response.data.results[0].geometry.location.lat;
-    var lng = response.data.results[0].geometry.location.lng;
-    var weatherUrl = 'https://api.darksky.net/forecast/' + process.env.API_KEY + '/' + lat + ',' + lng;
-    console.log(response.data.results[0].formatted_address);
-    return axios.get(weatherUrl);
+  if (response.data.status === 'ZERO_RESULTS') {
+    throw new Error('Unable to find that address.');
+  }
+  var lat = response.data.results[0].geometry.location.lat;
+  var lng = response.data.results[0].geometry.location.lng;
+  var weatherUrl = 'https://api.darksky.net/forecast/' + query + '/' + lat + ',' + lng;
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherUrl);
 }).then((response) => {
-    var temperature = response.data.currently.temperature;
-    var apparentTemperature = response.data.currently.apparentTemperature;
-    console.log("It is currently " + temperature + ", It feels like " + apparentTemperature + '.')
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log("It is currently " + temperature + ", It feels like " + apparentTemperature + '.')
 }).catch((e) => {
-    if (e.code === 'ECONNREFUSED') {
-        console.log('Unable to connect to API servers.');
-    } else {
-        console.log(e.message);
-    }
+  if (e.code === 'ECONNREFUSED') {
+    console.log('Unable to connect to API servers.');
+  } else {
+    console.log(e.message);
+  }
 });
